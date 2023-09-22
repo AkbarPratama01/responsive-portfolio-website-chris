bb87a10a50b2fd252b3ab6fa3aaf4731f27eb0a9
Akbar Pratama <akbarpra23@gmail.com>
2023-09-22 21:52:29 +0700

    update header versi mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,31 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle'); 
+const navClose = document.getElementById('nav-close');
 
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        console.log('ok');
+        navMenu.classList.toggle('show-menu');
+    });
+}
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        console.log('tes');
+        navMenu.classList.remove('show-menu')
+    });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu');
+    // when we click on each nav__link, we remove the menu-show class
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SWIPER PROJECTS ===============*/
 
